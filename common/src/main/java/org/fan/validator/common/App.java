f156155c593827310887b2870bab3fb29480dd7a
XiaoFan1519 <xiaofanms@outlook.com>
2016-10-19 23:47:49 +0800

    初始化提交。

---
+++ common/src/main/java/org/fan/validator/common/App.java
@@ -0,0 +1,13 @@
+package org.fan.validator.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
