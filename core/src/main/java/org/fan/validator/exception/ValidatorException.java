cbc3e60cb0ee5bbc9477ef484c1847ea4f1f067a
XiaoFan1519 <xiaofanms@outlook.com>
2016-10-22 21:59:27 +0800

    修改描述信息。

--- core/src/main/java/org/fan/validator/exception/ValidatorException.java
+++ core/src/main/java/org/fan/validator/exception/ValidatorException.java
@@ -13,7 +13,7 @@ public class ValidatorException extends RuntimeException {
     private static final long serialVersionUID = -7521481307777742644L;
     
     /**
-     * 禁用构造方法，要求抛出时必须明确指定异常原因
+     * 禁用无参的构造方法，要求抛出时必须明确指定异常原因
      */
     private ValidatorException()
     {
