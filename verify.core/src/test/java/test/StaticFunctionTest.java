9b545e011e11a951b9ed22c0c42a13341dbda4df
Fan <xiaofanms@outlook.com>
2017-05-17 13:57:51 +0800

    整合功能。

--- verify.core/src/test/java/test/StaticFunctionTest.java
+++ verify.core/src/test/java/test/StaticFunctionTest.java
@@ -5,8 +5,8 @@ package test;
 
 import org.junit.Test;
 
+import com.github.xiaofan1519.verify.core.Verify;
 import com.github.xiaofan1519.verify.utils.UnitTestUtil;
-import com.github.xiaofan1519.verify.utils.VerifyUtil;
 
 /**
  * 公用方法单元测试
@@ -19,15 +19,15 @@ public class StaticFunctionTest {
 	public void verifyNull()
 	{
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyNull(null);
+			Verify.verifyNull(null);
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyNull(1);
+			Verify.verifyNull(1);
 		}, false);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyNull("");
+			Verify.verifyNull("");
 		}, false);
 	}
 
@@ -35,11 +35,11 @@ public class StaticFunctionTest {
 	public void verifyEmpty()
 	{
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEmpty("");
+			Verify.verifyEmpty("");
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEmpty("1");
+			Verify.verifyEmpty("1");
 		}, false);
 	}
 	
@@ -47,19 +47,19 @@ public class StaticFunctionTest {
 	public void verifyLength()
 	{
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLen("", 1, 2);
+			Verify.verifyLen("", 1, 2);
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLen("123", 1, 2);
+			Verify.verifyLen("123", 1, 2);
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLen("1", 1, 2);
+			Verify.verifyLen("1", 1, 2);
 		}, false);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLen("12", 1, 2);
+			Verify.verifyLen("12", 1, 2);
 		}, false);
 	}
 	
@@ -67,23 +67,23 @@ public class StaticFunctionTest {
 	public void verifyLengthAllowNull()
 	{
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLenAllowNull("", 1, 2);
+			Verify.verifyLenAllowNull("", 1, 2);
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLenAllowNull("123", 1, 2);
+			Verify.verifyLenAllowNull("123", 1, 2);
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLenAllowNull("1", 1, 2);
+			Verify.verifyLenAllowNull("1", 1, 2);
 		}, false);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLenAllowNull("12", 1, 2);
+			Verify.verifyLenAllowNull("12", 1, 2);
 		}, false);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyLenAllowNull(null, 1, 2);
+			Verify.verifyLenAllowNull(null, 1, 2);
 		}, false);
 	}
 	
@@ -91,11 +91,11 @@ public class StaticFunctionTest {
 	public void verifyEnum()
 	{
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEnum("1", "2", "3");
+			Verify.verifyEnum("1", "2", "3");
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEnum("2", "2", "3");
+			Verify.verifyEnum("2", "2", "3");
 		}, false);
 	}
 	
@@ -103,15 +103,15 @@ public class StaticFunctionTest {
 	public void verifyEnumAllowNull()
 	{
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEnumAllowNull("1", "2", "3");
+			Verify.verifyEnumAllowNull("1", "2", "3");
 		}, true);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEnumAllowNull("2", "2", "3");
+			Verify.verifyEnumAllowNull("2", "2", "3");
 		}, false);
 		
 		UnitTestUtil.test(() -> {
-			VerifyUtil.verifyEnumAllowNull(null, "2", "3");
+			Verify.verifyEnumAllowNull(null, "2", "3");
 		}, false);
 	}
 	
